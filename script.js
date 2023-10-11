48c43654b96a35f057a9d5895da67b8a92e04854
Kevin Massongo <kevinmassongo196@gmail.com>
2023-10-11 21:12:12 +0100

    feat: retrieve html elements

---
+++ script.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll('button[type="button"]');
+const input = document.getElementById('input');
+const calcul = document.getElementById('calcul');
+const reset = document.getElementById('reset');
+const clear = document.getElementById('clear');
+const plus = document.getElementById('plus');
+const moins = document.getElementById('minus');
+const times = document.getElementById('times');
+const divideby = document.getElementById('divideby');
+const sign = document.getElementById('plusoumoins');
+const percentage = document.getElementById('percentage')
+const equals = document.getElementById('equals')
+
+let inputValeur = '';
+let textInput = '';
